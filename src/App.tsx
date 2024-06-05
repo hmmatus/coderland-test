384cb1d95de11f02b5f472db4903df32afbbad5b
Hector Matus <{ID}+{username}@users.noreply.github.com>
2024-06-05 12:06:56 -0600

    Fix store config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
 import MainStack from './navigator/main';
 import { NavigationContainer } from '@react-navigation/native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { setupStore } from './redux/store';
 const App = () => {
   const queryClient = new QueryClient();
+  const store = setupStore();
   return (
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
